ed6704d6f41261f9b62f76d1cf0af6882b18698e
Marilia <ramonap2013@gmail.com>
2024-03-11 22:39:46 -0500

    change

--- walker.js
+++ walker.js
@@ -12,13 +12,24 @@ class Walker {
 
     // directs the Walker object to take a step.
     step() {
-        let choice = floor(random(4)); // 0, 1, 2, or 3
+        // let choice = floor(random(4)); // 0, 1, 2, or 3
 
-        if (choice === 0) {
+        // if (choice === 0) {
+        //     this.x++;
+        // } else if (choice === 1) {
+        //     this.x--;
+        // } else if (choice === 2) {
+        //     this.y++;
+        // } else {
+        //     this.y--;
+        // }
+        let r = random(1);
+
+        if (r < 0.4) { // a 40% chance of moving to the right
             this.x++;
-        } else if (choice === 1) {
+        } else if (r < 0.6) {
             this.x--;
-        } else if (choice === 2) {
+        } else if (r < 0.8) {
             this.y++;
         } else {
             this.y--;
